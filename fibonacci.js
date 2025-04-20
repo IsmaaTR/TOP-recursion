b4cc1db1019106b4589c9d6da57a76adeb161f37
Isma <IsmaaTR@users.noreply.github.com>
2025-04-20 14:01:34 +0200

    fibonacci recursive

--- fibonacci.js
+++ fibonacci.js
@@ -13,8 +13,22 @@ function fibs(n) {
             num2 = arr[i];
         }
     }
-    console.log(arr);
     return arr;
 }
 
-fibs(8);
+function fibsRec(n) {
+    if (n === 0) {
+        return [];
+    } else if (n === 1) {
+        return [0];
+    } else if (n === 2) {
+        return [0, 1];
+    }
+
+    const arr = fibsRec(n - 1);
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+    return arr;
+}
+
+console.log(fibsRec(8));
+console.log(fibs(8));
